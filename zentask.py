8bdb05b182aa002771def4d58798839560a54c6c
aTomWeaver <atomweavermail@gmail.com>
2023-06-24 10:41:15 -0400

    object oriented refactor

--- zentask.py
+++ zentask.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 from datetime import datetime
+from pprint import pprint
 
 current_tasks_dict = json.load(open('./current.json', 'r'))
 
@@ -24,84 +25,111 @@ def reindex(dict):
         for key in keys:
             dict[i] = dict.pop(str(key))
             i += 1
-
-    # overwrite
-    with open('current.json', 'w', encoding='utf-8') as current:
-        json.dump(dict, current)
+    return dict
 
 
-def get_max_value_length(value):
+def get_max_value_length(value, task_dict):
+    '''returns the max length of a given value in a given task_dict'''
     max_value_length = 0
 
-    for key in current_tasks_dict.keys():
-        value_length = len(current_tasks_dict[key][value])
+    for key in task_dict.keys():
+        value_length = len(task_dict[key][value])
         if value_length > max_value_length:
             max_value_length = value_length
     return max_value_length
 
+# classes
 
-# create
-def add_task(new_task: dict):
-    '''Saves a new task to current.json from given dict of
-    {title, priority, context, project}.
-    '''
-    # copy and re-index current_task_dict
-    current_tasks_copy = current_tasks_dict.copy()
-    reindex(current_tasks_copy)
-    keys = get_int_key_list(current_tasks_copy)
-    new_task_index = max(keys) + 1
 
-    current_tasks_copy[str(new_task_index)] = new_task
-    with open('current.json', 'w', encoding='utf-8') as current:
-        json.dump(current_tasks_copy, current)
+class TaskList:
+    def __init__(self):
+        self.current_tasks_dict = json.load(open('./current.json', 'r'))
+
+    def refresh_current(self):
+        self.current_tasks_dict = json.load(open('./current.json', 'r'))
+
+    def write_to_current_json(self, file_to_write):
+        with open('current.json', 'w', encoding='utf-8') as current:
+            json.dump(file_to_write, current)
+
+    def archive_task(self, task):
+        '''appends task to archive.json'''
+        pass
+
+    def add_task(self, title, priority, context, project):
+        '''Saves a new task to current.json from passed props'''
+        # copy and re-index current_task_dict
+        current_tasks_copy = self.current_tasks_dict.copy()
+        current_tasks_copy = reindex(current_tasks_copy)
+        keys = get_int_key_list(current_tasks_copy)
+        new_task_index = max(keys) + 1
+
+        current_tasks_copy[str(new_task_index)] = Task(
+            title, priority, context, project).get_dict()
+        self.write_to_current_json(current_tasks_copy)
+
+    def delete_task(self, task):
+        pass
+
+    def list_current_tasks(self):
+        '''prints out tasks to the command line'''
+        self.refresh_current()
+        self.current_tasks_dict = reindex(self.current_tasks_dict)
+        max_title_lenth = get_max_value_length(
+            'title', self.current_tasks_dict) + 1
+        max_context_length = get_max_value_length(
+            'context', self.current_tasks_dict) + 1
+
+        for key in self.current_tasks_dict.keys():
+            task = self.current_tasks_dict[key]
+
+            title_right_pad = (max_title_lenth - len(task["title"])) * ' '
+            context_right_pad = (max_context_length -
+                                 len(task["context"])) * ' '
+
+            print_line = f'{key} ({task["priority"]}) {task["title"]}{title_right_pad}'
+
+            if task["context"]:
+                print_line += f'@{task["context"]}{context_right_pad}'
+            else:
+                print_line += f'{context_right_pad} '
+
+            if task["project"]:
+                print_line += f'+{task["project"]}'
+
+            print(print_line)
+
+    def complete_task(self, task):
+        '''moves task from current to archive with a completion date'''
+
+        timestamp = datetime.now().strftime('%Y/%m/%d %H:%M')
+        task['completed'] = timestamp
+
+        self.archive_task(task)
+        self.delete_task(task)
+
+        print(
+            f'\nTask completed: {task["title"]}\nCompletion Date: {timestamp}')
+        print(f'\n{task}')
     pass
 
-# read
+
+class Task:
+    def __init__(self, title, priority="u", context="", project=""):
+        self.title = title
+        self.priority = priority
+        self.context = context
+        self.project = project
+
+    def get_dict(self):
+        return {
+            "title": self.title,
+            "priority": self.priority,
+            "context": self.context,
+            "project": self.project
+        }
 
 
-def list_current_tasks():
-    current_tasks_dict = json.load(open('./current.json', 'r'))
-    reindex(current_tasks_dict)
-    max_title_lenth = get_max_value_length('title') + 1
-    max_context_length = get_max_value_length('context') + 1
-
-    for key in current_tasks_dict.keys():
-        task = current_tasks_dict[key]
-
-        title_right_pad = (max_title_lenth - len(task["title"])) * ' '
-        context_right_pad = (max_context_length - len(task["context"])) * ' '
-
-        line = f'{key} ({task["priority"]}) {task["title"]}{title_right_pad}'
-        if task["context"]:
-            line += f'@{task["context"]}{context_right_pad}'
-        else:
-            line += f'{context_right_pad} '
-        if task["project"]:
-            line += f'+{task["project"]}'
-        print(line)
-    pass
-
-
-def list_archived_tasks():
-    pass
-
-# delete
-
-
-def delete_task(task):
-    pass
-
-
-def complete_task(task):
-    # remove task from current
-    # append task to archive with completion date
-    timestamp = datetime.now().strftime('%Y/%m/%d %H:%M')
-    print(f'\nTask completed: {task}\nCompletion Date: {timestamp}')
-
-add_task({
-    "title": "check if work",
-    "priority": "a",
-    "context": "",
-    "project": "get working"
-})
-list_current_tasks()
+current_tasks = TaskList()
+current_tasks.list_current_tasks()
+current_tasks.print_task_list()
