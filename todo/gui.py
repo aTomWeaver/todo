e80e672112c28ae542876caef57f0a42f7ca9a8d
aTomWeaver <atomweavermail@gmail.com>
2023-06-28 05:57:16 -0400

    commit for work

---
+++ todo/gui.py
@@ -0,0 +1,12 @@
+from td import current_tasks
+from tkinter import *
+from tkinter import ttk
+
+# td.current_tasks.archive_task('8')
+# current_tasks.add_task('new task', 'a', '', 'todogui')
+# td.CLI.print_tasks(td.current_tasks.current_tasks_dict)
+
+root = Tk()
+root.geometry('500x500')
+
+root.mainloop()
